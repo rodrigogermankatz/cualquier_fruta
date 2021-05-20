6e2aecfe95eddee2c6a88f7618a94c9c16b9765a
Rodrigo Katz <rodrigog.katz@hotmail.com>
2021-05-20 20:50:20 -0300

    lo que se te cante... tiene que ser un mensaje descriptivo de lo que hiciste

---
+++ src/cualquier_fruta_1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : cualquier_fruta_1.c
+ Author      : Rodrigo Katz
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
